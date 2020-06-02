5172818af804aa19b1a27320a89a856cffe0af8e
Vlad Zarechnev <vlad.zarechnev.01@mail.ru>
2020-06-03 00:15:29 +0700

    Modified file main.c

--- src/main.c
+++ src/main.c
@@ -8,10 +8,52 @@
 int main()
 {
     int countStr;
-    char fileName[] = "input.txt";
+    char *p, bufer[255], fileName[] = "input.txt";
+    FILE* input;
+    countStr = get_count_strokes(fileName);
+    int i, j, isValid, rezults[countStr];
+    i = 0;
+    isValid = 1;
 
-    if (check_data_strokes(fileName) == 0) {
-        countStr = get_count_strokes(fileName);
+    input = fopen(fileName, "r");
+    p = fgets(bufer, 255, input);
+
+    while (p != NULL) {
+        rezults[i] = check_data_strokes(p);
+        i++;
+        p = fgets(bufer, 255, input);
+    }
+    fclose(input);
+    input = fopen(fileName, "r");
+    p = fgets(bufer, 255, input);
+
+    i = 0;
+    while (p != NULL) {
+        if (rezults[i] == 1) {
+            printf("Error: unknow figure \'");
+            for (j = 0; p[j] != '('; j++)
+                printf("%c", p[j]);
+            printf("\'\n");
+            isValid = 0;
+
+        } else if (rezults[i] == 2) {
+            printf("Error: invalid input format %s", p);
+            isValid = 0;
+
+        } else if (rezults[i] == 3) {
+            printf("Error: unknow figure \'");
+            for (j = 0; p[j] != '('; j++)
+                printf("%c", p[j]);
+            printf("\n");
+            printf("Error: invalid input format %s", p);
+            isValid = 0;
+        }
+        i++;
+        p = fgets(bufer, 255, input);
+    }
+    fclose(input);
+
+    if (isValid) {
         float coordsX[countStr], coordsY[countStr], rad[countStr];
 
         get_data(fileName, countStr, coordsX, coordsY, rad);
